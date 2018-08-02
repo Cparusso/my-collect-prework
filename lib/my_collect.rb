2ab9aa0f40956c7d57469d194a2df86246a496d9
Charlie Russo <cparusso@gmail.com>
2018-08-02 21:11:17 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -5,7 +5,7 @@ def my_collect(array)
   while i < array.length
     yield(array[i])
 
-    new_array << array[i].upcase.split(" ").first
+    new_array << array[i].split(" ").first
     i += 1
   end
 
